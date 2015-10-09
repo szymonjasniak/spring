ec65edf6a696da0560a828b4ed0c86baa4864b20
szymonjasniak <szymonjasniak@gmail.com>
2015-10-09 14:45:13 +0200

    using of Flyway to migrate DB

--- example-spring-boot-rest/src/main/java/javabeat/net/springboot/Application.java
+++ example-spring-boot-rest/src/main/java/javabeat/net/springboot/Application.java
@@ -1,5 +1,6 @@
 package javabeat.net.springboot;
 
+import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -15,7 +16,17 @@ import org.springframework.context.annotation.PropertySource;
 public class Application extends SpringBootServletInitializer {
 
     public static void main(final String[] args) {
+        
+        
+        
+        
         SpringApplication.run(Application.class, args);
+        
+        Flyway flyway = new Flyway();
+        flyway.setDataSource("jdbc:oracle:thin:@localhost:1521:xe", "szymonTest", "test");
+        flyway.setBaselineOnMigrate(true);
+        flyway.setValidateOnMigrate(true);
+        flyway.migrate();
     }
 
     @Override
